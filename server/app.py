fab752dbeb43ba51772b0ace809b26ee890eced3
christopherchege <chrisshege35@gmail.com>
2025-06-27 15:21:45 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,37 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants=Plant.query.all()
+        return [plant.to_dict() for plant in plants],200
+    
+    def post(self):
+        data= request.get_json()
+        try:
+            new_plant=Plant(
+                name=data['name'],
+                image=data['image'],
+                price=data['price']
+            )
+            db.session.add(new_plant)
+            db.session.commit()
+            return new_plant.to_dict(),201
+        except Exception as e:
+            return {"error":str(e)},400
+    
+api.add_resource(Plants,"/plants")
+
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant= Plant.query.get(id)
+        if plant:
+            return plant.to_dict(),200
+        return {"error":"Plant not found"},404
+    
         
+api.add_resource(PlantByID, "/plants/<int:id>")
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
